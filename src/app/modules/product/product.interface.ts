b09b151e72f198724d825f040f3b32c87a97c990
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-05-21 21:41:38 +0600

    create product.model.ts

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -17,25 +17,3 @@ export interface IProduct {
   variants: IVariants[];
   inventory: IInventory;
 }
-
-// {
-//     "name": "iPhone 13",
-//     "description": "A sleek and powerful smartphone with cutting-edge features.",
-//     "price": 999,
-//     "category": "Electronics",
-//     "tags": ["smartphone", "Apple", "iOS"],
-//     "variants": [
-//         {
-//             "type": "Color",
-//             "value": "Midnight Blue"
-//         },
-//         {
-//             "type": "Storage Capacity",
-//             "value": "256GB"
-//         }
-//     ],
-//     "inventory": {
-//         "quantity": 50,
-//         "inStock": true
-//     }
-// }
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,59 @@
+import { Schema } from "mongoose";
+import { IInventory, IProduct, IVariants } from "./product.interface";
+
+export const variantsSchema = new Schema<IVariants>({
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+
+export const inventorySchema = new Schema<IInventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export const productSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variants: {
+    type: [variantsSchema],
+    required: true,
+  },
+  inventory: {
+    type: inventorySchema,
+    required: true,
+  },
+});
