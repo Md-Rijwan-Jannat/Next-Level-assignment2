f48f33933431a55e46db795568a0da94cb5cc42b
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-05-21 22:27:40 +0600

    create product successfully and route and global error handle

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { ProductRouters } from "./app/modules/product/product.routes";
 const app = express();
@@ -13,6 +13,25 @@ app.use("/api/products", ProductRouters);
 
 app.get("/", (req, res) => {
   res.send("Product server is running!");
+});
+
+// Route error
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// global error handle
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err.headersSent) {
+    next();
+  }
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal server error",
+  });
 });
 
 export default app;
--- src/app/modules/product/product.controllers.ts
+++ src/app/modules/product/product.controllers.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { Product } from "./product.model";
+import { ProductServices } from "./product.servises";
 
 const createProduct = async (req: Request, res: Response) => {
   try {
-    const { product: productData } = await req.body();
-    const result = await Product.create(productData);
+    const { product: productData } = await req.body;
+    const result = await ProductServices.createProductIntoDB(productData);
     res.status(202).json({
       success: true,
       message: "Product created successfully",
