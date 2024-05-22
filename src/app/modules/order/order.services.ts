d5ad5007a4c7775a98003b752e4b61d43f25f849
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-05-22 19:27:50 +0600

    create a specific customer all order servise

--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -1,6 +1,7 @@
 import { IOrder } from "./order.interface";
 import { Order } from "./order.model";
 
+// -----> Create order into database
 const createOrderIntoDB = async (order: IOrder) => {
   try {
     const result = await Order.create(order);
@@ -14,20 +15,36 @@ const createOrderIntoDB = async (order: IOrder) => {
   }
 };
 
+// -----> Get all orders from database
 const getAllOrderFromDB = async () => {
   try {
     const result = await Order.find();
     if (!result) {
-      throw new Error("Order fetched filed!");
+      throw new Error("Orders fetched filed!");
     } else {
       return result;
     }
   } catch (error: any) {
-    throw new Error(error.message || "Order fetched filed!");
+    throw new Error(error.message || "Orders fetched filed!");
+  }
+};
+
+// -----> Get single customer all orders using email from database
+const getCustomerAllOrdersFromDB = async (email: string) => {
+  try {
+    const result = await Order.find({ $text: { $search: email } });
+    if (!result) {
+      throw new Error("Orders not found!");
+    } else {
+      return result;
+    }
+  } catch (error: any) {
+    throw new Error(error.message || "Orders not found!");
   }
 };
 
 export const OrderServices = {
   createOrderIntoDB,
   getAllOrderFromDB,
+  getCustomerAllOrdersFromDB,
 };
